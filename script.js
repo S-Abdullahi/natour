b94e1c022cad8a7706c8eaec16b11ec0a4bba588
Abdullahi Salawu <salabdullahi33@gmail.com>
2022-11-30 19:07:14 +0100

    quick fix

---
+++ script.js
@@ -0,0 +1,15 @@
+// 'use strict'
+// const navLink = document.querySelectorAll('.navigation__link')
+// const navIcon = document.querySelector('#navi-toggle')
+
+// for(let i = 0; i < navLink.length; i++){
+//     navLink[i].addEventListener('click', ()=>{
+//         document.querySelector('.navigation__background').classList.add('hidden')
+//         document.querySelector('.navigation__nav').classList.add('hidden')
+//     })
+// }
+
+// navIcon.addEventListener('click', ()=>{
+//     document.querySelector('.navigation__background').classList.toggle('hidden')
+//     document.querySelector('.navigation__nav').classList.toggle('hidden')
+// })
